144ee39b59c0652fa73f05df05e07e4f53b8bcb2
Ingrid <git@ingrids.email>
2021-12-23 01:15:55 +0100

    dwm: close windows with super + q, not w

--- dwm/config.h
+++ dwm/config.h
@@ -103,7 +103,7 @@ static Key keys[] = {
 	{ MODKEY,               XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,            XK_semicolon, zoom,           {0} },
 	{ MODKEY,               XK_Tab,    view,           {0} },
-	{ MODKEY,               XK_w,      killclient,     {0} },
+	{ MODKEY,               XK_q,      killclient,     {0} },
 	//{ MODKEY,               XK_t,      setlayout,      {.v = &layouts[0]} },
 	//{ MODKEY,               XK_f,      setlayout,      {.v = &layouts[1]} },
 	//{ MODKEY,               XK_m,      setlayout,      {.v = &layouts[2]} },
