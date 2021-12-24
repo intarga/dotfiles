e0ef54d56c9c347b7ca6747261d2fa04f6c01a6c
Ingrid <git@ingrids.email>
2021-12-24 17:53:00 +0100

    eww: monochrome icons in bar

--- eww/scripts/battery.sh
+++ eww/scripts/battery.sh
@@ -4,16 +4,27 @@ percent=`acpi | cut -d',' -f 2 | sed s/%// | cut -c 2-`
 state=`acpi | cut -d',' -f 1 | cut -c 12-`
 
 if [ $state = "Charging" ]; then
-    icon="🔌"
+    icon=""
 elif [ $state = "Discharging" ]; then
-    icon="🔋"
-    if [ $percent -lt 10 ]; then
-        icon="🪫"
+    if [ $percent -gt 80 ]; then
+        icon=""
+    elif [ $percent -gt 60 ]; then
+        icon=""
+    elif [ $percent -gt 40 ]; then
+        icon=""
+    elif [ $percent -gt 20 ]; then
+        icon=""
+    else
+        icon=""
     fi
 elif [ $state = "Charged" ]; then
-    icon="💡"
+    icon=""
 else
     icon="?"
 fi
 
-echo "(box :orientation \"v\" :valign \"end\" :space-evenly false (label :text \"$percent\") (label :text \"$icon\") (label :text \"~\"))"
+echo "(box :orientation \"v\" :valign \"end\" :space-evenly false (label :text \"$percent\") (label :class \"siji\" :text \"$icon\") (label :text \"~\"))"
+# icon="🔌"
+# icon="🔋"
+# icon="🪫"
+# icon="💡"
